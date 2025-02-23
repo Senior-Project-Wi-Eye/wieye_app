22407807869592890de5d6dc3bfedb7191060685
Tyler Truong <t_truong24@u.pacific.edu>
2025-02-23 00:52:51 -0800

    Added device screen and cards for devices on network(not connected to network)

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WieyeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WieyeApp extends StatelessWidget {
+  const WieyeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
-      home: const MyHomePage(title: 'Wi-Eye Demo'),
+      home: const HomeScreen(title: 'Wi-Eye Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _HomeScreenState extends State<HomeScreen> {
 
   int _selectedIndex = 0;
 
@@ -43,11 +36,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // List of screen
   static final List<Widget> _screens = <Widget>[
     const Center(child: Text('Home Screen')),
-    const infoScreen(),
+    const DeviceScreen(),
+    const InfoScreen(),
   ];
 
+  // Navigation Bar: How app move bettween screen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,11 +54,6 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: _screens[_selectedIndex],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
@@ -70,6 +61,10 @@ class _MyHomePageState extends State<MyHomePage> {
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.devices),
+            label: 'Device',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.info),
@@ -81,13 +76,74 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class infoScreen extends StatelessWidget {
-  const infoScreen({Key? key}) : super(key: key);
+class DeviceScreen extends StatelessWidget {
+  const DeviceScreen({super.key});
+
+  // List of decives on network
+  final List<Map<String, String>> devices = const [
+    {"name": "Windows", "type": "monitor", "ip": "10.0.0.21"},
+    {"name": "iPhone", "type": "smartphone", "ip": "10.0.0.22"},
+    {"name": "Laptop", "type": "laptop", "ip": "10.0.0.23"},
+    {"name": "Android", "type": "smartphone", "ip": "10.0.0.24"},
+    {"name": "iPad", "type": "tablet", "ip": "10.0.0.25"},
+    {"name": "Unidentified Device", "type": "unknown", "ip": "10.0.0.26"},
+  ];
+
+  // Assign icons to devices
+  IconData getDeviceIcon(String type) {
+    switch (type) {
+      case "smartphone":
+        return Icons.smartphone;
+      case "monitor":
+        return Icons.monitor;
+      case "laptop":
+        return Icons.laptop;
+      case "tablet":
+        return Icons.tablet;
+      case "unknown":
+        return Icons.device_unknown;
+      default:
+        return Icons.devices;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Info')),
+      appBar: AppBar(title: const Text("Devices")),
+      body: Column(
+        children: [
+          // Device List
+          Expanded(
+            child: ListView.builder(
+              itemCount: devices.length,
+              itemBuilder: (context, index) {
+                var device = devices[index];
+                return Card(
+                  child: ListTile(
+                    leading: Icon(getDeviceIcon(device["type"]!), size: 40),
+                    title: Text(device["name"]!),
+                    subtitle: Text("IP Address: ${device["ip"]}"),
+                    trailing: const Icon(Icons.circle, color: Colors.green, size: 14),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class InfoScreen extends StatelessWidget {
+  const InfoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text('Info'))),
       body: const Center(
         child: Text('This is the Info Screen'),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:wieye_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WieyeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
